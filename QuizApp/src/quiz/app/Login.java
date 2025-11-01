2e1e122660c586d9dcb3cb19a12930a78cad788e
HarshuChauhan <152696313+HarshuChauhan@users.noreply.github.com>
2025-11-01 12:49:00 +0530

    Add files via upload

---
+++ QuizApp/src/quiz/app/Login.java
@@ -0,0 +1,87 @@
+package quiz.app;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    JTextField text;
+    JButton Next, Back;
+    Login(){
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+//        For Quiz Test Image
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.png"));
+        Image i = i1.getImage().getScaledInstance(550,500,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i);
+        JLabel image = new JLabel(i2);
+        image.setBounds(450,0,550,500);
+        add(image);
+
+//        For Heading "QUIZ TEST"
+        JLabel heading = new JLabel("QUIZ TEST");
+        heading.setBounds(140,60,300,45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD,40));
+        heading.setForeground(new Color(22,100,54));
+        add(heading);
+
+//        To show "Enter Your Name"
+        JLabel name = new JLabel("Enter Your Name");
+        name.setBounds(160,150,300,20);
+        name.setFont(new Font("Mongolian Baiti",Font.BOLD,18));
+        name.setForeground(new Color(22,100,54));
+        add(name);
+
+//        textField to get the Name
+        text = new JTextField();
+        text.setBounds(80,200,300,25);
+        text.setFont(new Font("Times New Roman",Font.BOLD,18));
+        add(text);
+
+//        Button to go to Next Page
+        Next = new JButton("Next");
+        Next.setBounds(100,270,120,25);
+        Next.setBackground(new Color(22,100,54));
+        Next.setForeground(Color.WHITE);
+        Next.addActionListener(this);
+        add(Next);
+
+//        Button to go to Previous Page
+        Back = new JButton("Back");
+        Back.setBounds(250,270,120,25);
+        Back.setBackground(new Color(22,100,54));
+        Back.setForeground(Color.WHITE);
+        Back.addActionListener(this);
+        add(Back);
+
+
+
+        setSize(1000, 500);
+        setLocation(300, 150);
+        setUndecorated(true);
+        setVisible(true);
+    }
+
+
+//    To Implement Action Listener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        For Next Button
+        if(e.getSource() == Next){
+            String name = text.getText();
+            setVisible(false);
+            new Rules(name);
+
+        }
+//        For Back Button
+        else if(e.getSource() == Back){
+            System.exit(50);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+
+    }
+}
